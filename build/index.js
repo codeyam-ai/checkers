c66b4b0ddd0f4a0b49dd52ce097ee796d8724237
Jared Cosulich <jared.cosulich@gmail.com>
2022-12-18 08:22:24 -0500

    new contract

--- build/index.js
+++ build/index.js
@@ -67,7 +67,7 @@ module.exports = {
 }
 },{"./constants":2,"./utils":6}],2:[function(require,module,exports){
 module.exports = {
-    contractAddress: "0x471a140c713395c8ba83f3da52570c9e954a808d",
+    contractAddress: "0x1622538c13f072501b30351dc3f2c529408c91f9",
     piece: (color, king) => (`
         <svg width="44" height="44" viewBox="0 0 525 525" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_d_359_466)">
